910fe27a7ac484d969a4b688876c68a6948599cb
Deepak345 <senapati.dipak97@gmail.com>
2018-01-29 16:44:22 +0530

    datatable, forgot password styling

--- xtasy-backend/controllers/admin.js
+++ xtasy-backend/controllers/admin.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 const UserModel = require('../models/user');
 
 
-var displayAll = function(req,res) {
-  if (req.session.user && req.session.user == "admin") {
-    var query = UserModel.find();
-    query.select('name emailid college xtasyid');
-  
-    query.exec(function(err, user) {
-      if(err) throw err;
-      console.log(user);
-      res.render("pages/admin" , {"users" : user})
-    });
-  } else {
-    res.redirect("/login?action=4");
-  }
+var displayAll = function(req, res) {
+    if (req.session.user && req.session.user == "admin") {
+        var query = UserModel.find({ isVerified: true });
+        query.select('name emailid college xtasyid');
+
+        query.exec(function(err, user) {
+            if (err) throw err;
+            console.log(user);
+            res.render("pages/admin", { "users": user })
+        });
+    } else {
+        res.redirect("/login?action=4");
+    }
 
 }
 
