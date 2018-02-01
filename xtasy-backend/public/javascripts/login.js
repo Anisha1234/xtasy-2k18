a505124327d293d3bf2524513e8f17212dd11d7f
Deepak345 <senapati.dipak97@gmail.com>
2018-02-01 20:14:34 +0530

    login page url,profile dark theme to light

--- xtasy-backend/public/javascripts/login.js
+++ xtasy-backend/public/javascripts/login.js
@@ -7,14 +7,15 @@ $(document).ready(function() {
         var urlParams = new URLSearchParams(window.location.search);
         console.log(urlParams.has('action'));
         if (urlParams.has('action')) {
+            var new_url = location.protocol + "//" + location.host + location.pathname;
             if (urlParams.get('action') === "1") alert("You have been successfully verified");
             if (urlParams.get('action') === "2") alert("You have not registered");
             if (urlParams.get('action') === "3") alert("You couldn't be verified");
-            if (urlParams.get('action') === "4") alert("Session expired! You need to login");
+            if (urlParams.get('action') === "4") alert("You need to login");
             if (urlParams.get('action') === "5") alert("Successfully logged out");
             if (urlParams.get('action') === "6") alert("Your password has been successfully reset");
             if (urlParams.get('action') === "7") alert("A mail has been sent to you");
-
+            window.history.replaceState({}, document.title, new_url);
         }
     }
 
