e7edb21e53614ac71ead5529408dc505973e48c4
Deepak345 <senapati.dipak97@gmail.com>
2018-02-19 12:31:19 +0530

    events gifs, ca page

--- xtasy-backend/routes/routes.js
+++ xtasy-backend/routes/routes.js
@@ -4,43 +4,47 @@ var admin = require('../controllers/admin');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('./pages/index');
+    res.render('./pages/index');
 });
 
-router.get("/login" , function(req, res,next){
-  if(req.session.user) {
-    if(req.session.user === "admin") res.redirect('/admin');
-    else res.redirect('/profile');
-  } else res.render("./pages/login");
+router.get("/login", function(req, res, next) {
+    if (req.session.user) {
+        if (req.session.user === "admin") res.redirect('/admin');
+        else res.redirect('/profile');
+    } else res.render("./pages/login");
 });
 
-router.get("/admin" , admin.displayAll);
+router.get("/admin", admin.displayAll);
 
 router.get("/profile", function(req, res) {
-  if(req.session.user) {
-    if(req.session.user === "admin") res.redirect('/admin');
-    else res.render('./pages/profile' , { "details" : req.session.user });
-  } else res.redirect("/login");
+    if (req.session.user) {
+        if (req.session.user === "admin") res.redirect('/admin');
+        else res.render('./pages/profile', { "details": req.session.user });
+    } else res.redirect("/login");
 });
 
 /*router.get("/reset", function(req,res,next){
   res.render("./pages/passwordreset")
 }) */
 
-router.get("/forgot", function (req,res,next) {
-  res.render("./pages/forgot");
+router.get("/forgot", function(req, res, next) {
+    res.render("./pages/forgot");
 });
 
-router.get("/events", function(req,res,next){
-  res.render("./pages/events");
+router.get("/events", function(req, res, next) {
+    res.render("./pages/events");
 });
 
-router.get("/gallery" , function(req,res,next){
-  res.render("./pages/gallery");
+router.get("/gallery", function(req, res, next) {
+    res.render("./pages/gallery");
 });
 
-router.get("/team" , function(req,res,next){
-  res.render("./pages/team");
+router.get("/team", function(req, res, next) {
+    res.render("./pages/team");
+});
+
+router.get("/ca", function(req, res, next) {
+    res.render("./pages/ca");
 });
 
 module.exports = router;
